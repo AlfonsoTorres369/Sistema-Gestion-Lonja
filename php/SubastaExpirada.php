a216b686fbb3dd11600e29f62030b09faca582f7
eromeroa <e.romeroa@alumnos.upm.es>
2019-04-23 18:37:32 +0200

    modificada pagina SubastaExpirada y estilos de la misma

---
+++ php/SubastaExpirada.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Página principal de Aquabid">
+    <meta name="author" content="Miguel Ángel Pérez, Eric Romero, Alberto Sastre, Alfonso Torres">
+
+    <title>Subasta expirada</title>
+
+    <link rel="shortcut icon" href="../images/Aquabid.png">
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa:400,700" rel="stylesheet">
+
+    <!-- JavaScript -->
+    <script language="javascript" type="text/javascript" src="../js/Captura.js"></script>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link href="../css/lonja.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/RegistroCliente.css">
+
+
+</head>
+
+<body id="bprincipal ">
+    <!-- Navigation -->
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top nnavbar">
+            <div class="container">
+                <a class="navbar-brand" href="Principal.php"><img src="../images/Aquabid.png" width="55px"></a>
+                <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarResponsive">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item-principal">
+                            <a class="nav-link" href="Principal.php">Home</a>
+                        </li>
+                        <li class="nav-item-principal">
+                            <a class="nav-link" href="Captura.php">Captura</a>
+                        </li>
+                        <li class="nav-item-principal">
+                            <a class="nav-link" href="Subastas.php">Subastas</a>
+                        </li>
+                        <li class="nav-item-principal">
+                            <a class="nav-link" href="SubastasExpress.php">Subastas Express</a>
+                        </li>
+                        <li class="nav-item-principal">
+                            <a class="nav-link" href="Cesta.php">Cesta</a>
+                        </li>
+                    </ul>
+
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item-principal">
+                            <a class="nav-link" href="InformacionCliente.php">Info</a>
+                        </li>
+                        <li class="nav-item-principal">
+                            <a class="nav-link" href="PerfilCliente.php">Perfil</a>
+                        </li>
+                        <li class="nav-item-principal">
+                            <a class="nav-link" href="logout-cliente.php">Cerrar Sesión</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <!-- Page Content -->
+    <br>
+    <br>
+    <div id="expirada" class="shadow-lg container">
+        <br>
+        <h1 class="text-center">Subasta expirada</h1>
+        <c>La subasta ha terminado sin que ninguno de los participantes haya adquirido el lote. El lote se pondrá a la venta en una subasta express próximamente</c>
+        <br>
+        <br>
+        <h4 class="text-center">Gracias por participar</h4>
+        <br>
+        <button type="submit" class="btn btn-lg btn-primary btn-block btn-login text-uppercase font-weight-bold mb-2" id="botonComprar" name="botonComprar">Salir</button>
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+    <footer class="npadding bg-dark">
+        <div class="container">
+            <p class="m-0 text-center text-white">Copyright &copy; Aquabid 2019</p>
+        </div>
+    </footer>
+</body>
+
+</html>
